2423af9bab7cf534248c112b13f39bb15cca4c4c
Jhaym3s <jhaymescode@gmail.com>
2023-02-02 11:13:25 +0100

    http with interfaces

--- hello_world.go
+++ hello_world.go
@@ -1,30 +1,32 @@
 package main
 
-import "fmt"
+//import "fmt"
 
 
 
 
 func main() {
-	newDeck := readFromDrive("decks")
-	fmt.Println(newDeck.printCards())
+	// newDeck := readFromDrive("decks")
+	// fmt.Println(newDeck.printCards())
 
-	wallet := Wallet{currency: "Bitcoin",balance: 30}
-	wallet.Deposit(50)
-	fmt.Printf("wallet sum %d", wallet.sum())
-	jhaymes := Person{name: "Paulinus"}
-	jhaymes.updateName("James")
-	fmt.Println(jhaymes.printName())
+	// wallet := Wallet{currency: "Bitcoin",balance: 30}
+	// wallet.Deposit(50)
+	// fmt.Printf("wallet sum %d", wallet.sum())
+	// jhaymes := Person{name: "Paulinus"}
+	// jhaymes.updateName("James")
+	// fmt.Println(jhaymes.printName())
 
-	names := map[int]string{
+	// names := map[int]string{
 		
-		0: "jhaymes",
-		1: "cool",
-		2: "name",
-		4: "last",
+	// 	0: "jhaymes",
+	// 	1: "cool",
+	// 	2: "name",
+	// 	4: "last",
 
-	}
-	iterateMaps(names)
-	printMaps(names)
-	iterateMaps(names)
+	// }
+	// iterateMaps(names)
+	// printMaps(names)
+	// iterateMaps(names)
+	chatGPT()
+	getRequestCustom()
 }
